e25c544793fb349da32484bd1ab369e3b279c19f
Guilherme <gededeus@gmail.com>
2025-08-28 21:36:30 -0300

    Create IMapeador.java

---
+++ IMapeador.java
@@ -0,0 +1,12 @@
+package br.edu.faculdade.poo2;
+
+public interface IMapeador {
+
+    boolean inserir(Object obj);
+
+    boolean atualizar(Object obj);
+
+    boolean excluir(Object obj);
+
+    Object obter(Oid oid);
+}
