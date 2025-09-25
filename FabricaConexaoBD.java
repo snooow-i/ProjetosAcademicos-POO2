74f4ae6497d748a6f4a303957505c8a94b16fb90
Guilherme <gededeus@gmail.com>
2025-09-25 18:11:37 -0300

    Update FabricaConexaoBD.java

--- FabricaConexaoBD.java
+++ FabricaConexaoBD.java
@@ -5,26 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class FabricaConexaoBD {
-
     private static final String URL = "jdbc:postgresql://localhost:5432/faculdade"; 
     private static final String USER = "postgres"; 
-    private static final String PASSWORD = "postgres"; 
+    private static final String PASSWORD = "postgres";
 
     private static FabricaConexaoBD instancia = null;
-    private Connection conexao = null;
 
     private FabricaConexaoBD() {
         try {
             Class.forName("org.postgresql.Driver");
-            
-            this.conexao = DriverManager.getConnection(URL, USER, PASSWORD);
-            
         } catch (ClassNotFoundException e) {
-            System.err.println("Driver JDBC do PostgreSQL não encontrado.");
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            System.err.println("Falha ao conectar ao banco de dados: " + e.getMessage());
-            throw new RuntimeException(e);
+            throw new RuntimeException("Driver JDBC do PostgreSQL não encontrado.", e);
         }
     }
 
@@ -35,9 +26,9 @@ public class FabricaConexaoBD {
         return instancia;
     }
 
-    public Connection getConexao() {
-        return this.conexao;
+    public Connection getConexao() throws SQLException {
+        Connection novaConexao = DriverManager.getConnection(URL, USER, PASSWORD);
+        novaConexao.setAutoCommit(false);
+        return novaConexao;
     }
-
 }
-
