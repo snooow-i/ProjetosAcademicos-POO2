bd47cd26527ed977f1595c219bc98200d3cf6ad2
Guilherme <gededeus@gmail.com>
2025-09-25 18:14:56 -0300

    Update Persistencia.java

--- Persistencia.java
+++ Persistencia.java
@@ -1,11 +1,9 @@
 package poo2;
 
 public class Persistencia {
-
     private static Persistencia instancia = null;
 
-    private Persistencia() {
-    }
+    private Persistencia() {}
 
     public static Persistencia obterInstancia() {
         if (instancia == null) {
@@ -14,18 +12,37 @@ public class Persistencia {
         return instancia;
     }
 
+    public boolean inserir(Object objeto) {
+        String nomeClasse = objeto.getClass().getSimpleName();
+        IMapeador mapeador = FabricaDeMapeador.obterInstancia().obterMapeador(nomeClasse);
+
+        if (mapeador != null) {
+            return mapeador.inserir(objeto);
+        }
+        
+        System.err.println("Nenhum mapeador encontrado para a classe: " + nomeClasse);
+        return false;
+    }
+
     public Object obter(Oid oid, Class<?> classe) {
+        String nomeClasse = classe.getSimpleName();
+        IMapeador mapeador = FabricaDeMapeador.obterInstancia().obterMapeador(nomeClasse);
+
+        if (mapeador != null) {
+            return mapeador.obter(oid);
+        }
+
+        System.err.println("Nenhum mapeador encontrado para a classe: " + nomeClasse);
         return null;
     }
-
-    public void inserir(Object objeto) {
-        
+    
+    public boolean atualizar(Object objeto) {
+        System.out.println("Método ATUALIZAR ainda não implementado.");
+        return false;
     }
 
-    public void atualizar(Object objeto) {
+    public boolean excluir(Object objeto) {
+        System.out.println("Método EXCLUIR ainda não implementado.");
+        return false;
     }
-
-    public void excluir(Object objeto) {
-    }
-
 }
