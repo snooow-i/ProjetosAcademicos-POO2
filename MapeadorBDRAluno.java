f67e91c950021580935ca6417c5f7670a3c41a27
Guilherme <gededeus@gmail.com>
2025-09-25 18:13:07 -0300

    Update MapeadorBDRAluno.java

--- MapeadorBDRAluno.java
+++ MapeadorBDRAluno.java
@@ -1,93 +1,43 @@
 package poo2;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class MapeadorBDRAluno implements IMapeador {
-
-    private Connection conexao;
+public class MapeadorBDRAluno extends MapeadorDeBDRAbstrato {
 
     public MapeadorBDRAluno() {
-        this.conexao = FabricaConexaoBD.obterInstancia().getConexao();
+        super("aluno");
     }
 
     @Override
-    public boolean inserir(Object obj) {
+    protected String obterNomesColunasInsert() {
+        return "matricula, nome";
+    }
+
+    @Override
+    protected String obterInterrogacoesInsert() {
+        return "?, ?";
+    }
+
+    @Override
+    protected void preencherParametrosInsert(PreparedStatement stm, Object obj) throws SQLException {
         Aluno aluno = (Aluno) obj;
-        String sql = "INSERT INTO aluno (matricula, nome) VALUES (?, ?)";
-        
-        try {
-            PreparedStatement stm = conexao.prepareStatement(sql);
-            stm.setInt(1, aluno.getMatricula());
-            stm.setString(2, aluno.getNome());
-
-            stm.executeUpdate();
-            stm.close();
-
-            buscarId(aluno);
-
-            return true;
-
-        } catch (SQLException e) {
-            System.err.println("Erro ao inserir aluno: " + e.getMessage());
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    private void buscarId(Aluno aluno) throws SQLException {
-        String sql = "SELECT id FROM aluno WHERE matricula = ?";
-        
-        PreparedStatement stm = conexao.prepareStatement(sql);
         stm.setInt(1, aluno.getMatricula());
-        
-        ResultSet rs = stm.executeQuery();
-        if (rs.next()) {
-            aluno.setId(rs.getLong("id")); 
-        }
-        rs.close();
-        stm.close();
+        stm.setString(2, aluno.getNome());
+    }
+    
+    @Override
+    protected void definirId(Object obj, long id) {
+        ((Aluno) obj).setId(id);
     }
 
     @Override
-    public Object obter(Oid oid) {
-        String sql = "SELECT * FROM aluno WHERE id = ?";
-        Aluno aluno = null;
-        
-        try {
-            PreparedStatement stm = conexao.prepareStatement(sql);
-            long id = Long.parseLong(oid.getString());
-            stm.setLong(1, id);
-
-            ResultSet rs = stm.executeQuery();
-
-            if (rs.next()) {
-                aluno = new Aluno();
-                aluno.setId(rs.getLong("id"));
-                aluno.setMatricula(rs.getInt("matricula"));
-                aluno.setNome(rs.getString("nome"));
-            }
-            
-            rs.close();
-            stm.close();
-
-        } catch (SQLException | NumberFormatException e) {
-            System.err.println("Erro ao obter aluno: " + e.getMessage());
-            e.printStackTrace();
-        }
-        
+    protected Object construirObjeto(ResultSet rs) throws SQLException {
+        Aluno aluno = new Aluno();
+        aluno.setId(rs.getLong("id"));
+        aluno.setMatricula(rs.getInt("matricula"));
+        aluno.setNome(rs.getString("nome"));
         return aluno;
-    }
-
-    
-    public boolean atualizar(Object obj) {
-        return false;
-    }
-
-    
-    public boolean excluir(Object obj) {
-        return false;
     }
 }
